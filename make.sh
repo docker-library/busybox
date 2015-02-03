c10141bea6ea8fe27215ece8298801e6bb6b39d4
Tianon Gravi <admwiggin@gmail.com>
2015-02-02 23:18:52 -0700

    Refactor to match upstream structure better

--- make.sh
+++ make.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -e
 
-cd "$(readlink -f "$(dirname "$BASH_SOURCE")")"
+cd "$(readlink -f "$(dirname "$BASH_SOURCE")")/upstream"
 
 set -x
-docker build -t busybox:builder builder
+docker build -t busybox:builder - < Dockerfile.builder
 docker run --rm busybox:builder tar cC rootfs-bin . | xz -z9 > busybox.tar.xz
 docker build -t busybox .
