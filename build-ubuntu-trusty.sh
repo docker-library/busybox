727cef357be5a518f473f6b879d693608c782dc2
Tianon Gravi <admwiggin@gmail.com>
2015-09-14 16:47:32 -0700

    Run a simple "ping" that has to lookup a DNS record to each build script to ensure proper functionality

--- build-ubuntu-trusty.sh
+++ build-ubuntu-trusty.sh
@@ -7,4 +7,5 @@ set -x
 docker build -t busybox:ubuntu-builder --pull - < Dockerfile.builder
 docker run --rm busybox:ubuntu-builder tar c . | xz -z9 > busybox.tar.xz
 docker build -t busybox:ubuntu .
-docker run -it --rm busybox:ubuntu sh -xec 'true'
+docker run --rm busybox:ubuntu sh -xec 'true'
+docker run --rm busybox:ubuntu ping -c 1 google.com
--- build-upstream.sh
+++ build-upstream.sh
@@ -7,4 +7,5 @@ set -x
 docker build -t busybox:builder --pull - < Dockerfile.builder
 docker run --rm busybox:builder tar cC rootfs . | xz -z9 > busybox.tar.xz
 docker build -t busybox .
-docker run -it --rm busybox sh -xec 'true'
+docker run --rm busybox sh -xec 'true'
+docker run --rm busybox ping -c 1 google.com
