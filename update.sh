f8263f8fcde3186422646f6e3caee8b181892664
Tianon Gravi <admwiggin@gmail.com>
2015-12-07 16:34:36 -0800

    Make update.sh slightly faster to complete

--- update.sh
+++ update.sh
@@ -6,9 +6,12 @@ cd "$(dirname "$(readlink -f "$BASH_SOURCE")")"
 upstreamVersion="$(curl -fsSL --compressed 'http://busybox.net/downloads/' | grep -E '<a href="busybox-[0-9][^"/]*.tar.bz2"' | sed -r 's!.*<a href="busybox-([0-9][^"/]*).tar.bz2".*!\1!' | sort -V | tail -1)"
 
 ubuntuImage="$(awk 'toupper($1) == "FROM" { print $2 }' ubuntu/Dockerfile.builder)"
-ubuntuVersion="$(docker run --rm ubuntu:trusty bash -c 'apt-get update -qq && apt-cache show busybox-static | awk -F ": " "\$1 == \"Version\" { print \$2; exit }"')"
+ubuntuVersion="$(docker run --rm ubuntu:trusty bash -ec '
+	sed -i "s/^deb-src/#&/g" /etc/apt/sources.list
+	apt-get update -qq
+	apt-cache show busybox-static | awk -F ": " "\$1 == \"Version\" { print \$2; exit }"
+')"
 
 set -x
 sed -ri 's/^(ENV BUSYBOX_VERSION) .*/\1 '"$upstreamVersion"'/;' upstream/Dockerfile.builder
 sed -ri 's/^(ENV BUSYBOX_VERSION) .*/\1 '"$ubuntuVersion"'/;' ubuntu/Dockerfile.builder
-#./make.sh
