13f14c6b73f4baf975b725fd12952d3c7272c410
Tianon Gravi <admwiggin@gmail.com>
2017-06-15 10:55:09 -0700

    Update "generate-stackbrew-library.sh" to use our new "dist-xyz" branches

--- generate-stackbrew-library.sh
+++ generate-stackbrew-library.sh
@@ -1,61 +1,53 @@
 #!/bin/bash
-set -eu
-
-latest=
-# prefer uclibc, but if it's unavailable use glibc if possible since it's got less "edge case" behavior, especially around DNS
-for try in \
-	uclibc \
-	glibc \
-	musl \
-; do
-	if [ -d "$try" ]; then
-		latest="$try"
-		break
-	fi
-done
-if [ -z "$latest" ]; then
-	echo >&2 'error: cannot determine a good choice for "latest"!'
-	exit 1
-fi
+set -Eeuo pipefail
 
 self="$(basename "$BASH_SOURCE")"
 cd "$(dirname "$(readlink -f "$BASH_SOURCE")")"
 
-variants=( */ )
-variants=( "${variants[@]%/}" )
+gitHubUrl='https://github.com/docker-library/busybox'
+#rawGitUrl="$gitHubUrl/raw"
+rawGitUrl="${gitHubUrl//github.com/cdn.rawgit.com}" # we grab a lot of tiny files, and rawgit's CDN is more consistently speedy on a cache hit than GitHub's
 
-# get the most recent commit which modified any of "$@"
-fileCommit() {
-	git log -1 --format='format:%H' --branches -- "$@"
-}
+# prefer uclibc, but if it's unavailable use glibc if possible since it's got less "edge case" behavior, especially around DNS
+variants=(
+	uclibc
+	glibc
+	musl
+)
+# (order here determines "preference" for representing "latest")
 
-# get the most recent commit which modified "$1/Dockerfile" or any file COPY'd from "$1/Dockerfile"
-dirCommit() {
-	local dir="$1"; shift
-	(
-		cd "$dir"
-		dfCommit="$(fileCommit Dockerfile)"
-		fileCommit \
-			Dockerfile \
-			$(git show "$dfCommit":./Dockerfile | awk '
-				toupper($1) ~ /^(COPY|ADD)$/ {
-					for (i = 2; i < NF; i++) {
-						print $i
-					}
-				}
-			')
-	)
-}
+archMaps=( $(
+	git ls-remote --heads "${gitHubUrl}.git" \
+		| awk -F '[\t/]' '$4 ~ /^dist-/ { gsub(/^dist-/, "", $4); print $4 "=" $1 }' \
+		| sort
+) )
+arches=()
+declare -A archCommits=()
+for archMap in "${archMaps[@]}"; do
+	arch="${archMap%%=*}"
+	commit="${archMap#${arch}=}"
+	arches+=( "$arch" )
+	archCommits[$arch]="$commit"
+done
 
+selfCommit="$(git log --format='format:%H' -1)"
 cat <<-EOH
-# this file is generated via https://github.com/docker-library/busybox/blob/$(fileCommit "$self")/$self
+# this file is generated via $gitHubUrl/blob/$selfCommit/$self
 
 Maintainers: Tianon Gravi <admwiggin@gmail.com> (@tianon),
              Joseph Ferguson <yosifkit@gmail.com> (@yosifkit),
              Jérôme Petazzoni <jerome@docker.com> (@jpetazzo)
-GitRepo: https://github.com/docker-library/busybox.git
-GitFetch: refs/heads/dist
+GitRepo: $gitHubUrl.git
+GitCommit: $selfCommit
 EOH
+for arch in "${arches[@]}"; do
+	commit="${archCommits[$arch]}"
+	cat <<-EOA
+		# $gitHubUrl/tree/dist-${arch}
+		${arch}-GitFetch: refs/heads/dist-${arch}
+		${arch}-GitCommit: $commit
+	EOA
+done
 
 # prints "$2$1$3$1...$N"
 join() {
@@ -64,34 +56,57 @@ join() {
 	echo "${out#$sep}"
 }
 
+# pre-flight sanity checks
+fullVersion=
 for variant in "${variants[@]}"; do
-	[ -f "$variant/Dockerfile.builder" ] || continue
+	[ -f "$variant/Dockerfile.builder" ]
+	oldVersion="$fullVersion"
+	fullVersion="$(awk '$1 == "ENV" && $2 == "BUSYBOX_VERSION" { print $3; exit }' "$variant/Dockerfile.builder")"
+	[ -n "$fullVersion" ]
+	[ "$fullVersion" = "${oldVersion:-$fullVersion}" ]
+done
+versionAliases=()
+while [ "${fullVersion%.*}" != "$fullVersion" ]; do
+	versionAliases+=( $fullVersion )
+	fullVersion="${fullVersion%.*}"
+done
+versionAliases+=(
+	$fullVersion
+	latest
+)
 
-	commit="$(dirCommit "$variant")"
-
-	fullVersion="$(git show "$commit":"$variant/Dockerfile.builder" | awk '$1 == "ENV" && $2 == "BUSYBOX_VERSION" { print $3; exit }')"
-
-	versionAliases=()
-	while [ "${fullVersion%.*}" != "$fullVersion" ]; do
-		versionAliases+=( $fullVersion )
-		fullVersion="${fullVersion%.*}"
-	done
-	versionAliases+=(
-		$fullVersion
-		latest
-	)
-
+declare -A archLatest=()
+for variant in "${variants[@]}"; do
 	variantAliases=( "${versionAliases[@]/%/-$variant}" )
 	variantAliases=( "${variantAliases[@]//latest-/}" )
 
-	if [ "$variant" = "$latest" ]; then
-		variantAliases+=( "${versionAliases[@]}" )
-	fi
+	variantArches=()
+	for arch in "${arches[@]}"; do
+		archCommit="${archCommits[$arch]}"
+		if wget --quiet --spider -O /dev/null -o /dev/null "$rawGitUrl/$archCommit/$variant/busybox.tar.xz"; then
+			variantArches+=( "$arch" )
+			if [ -z "${archLatest[$arch]:-}" ]; then
+				archLatest[$arch]="$variant"
+			fi
+		fi
+	done
 
 	echo
 	cat <<-EOE
 		Tags: $(join ', ' "${variantAliases[@]}")
-		GitCommit: $commit
+		Architectures: $(join ', ' "${variantArches[@]}")
 		Directory: $variant
 	EOE
+done
+
+echo
+cat <<-EOE
+	Tags: $(join ', ' "${versionAliases[@]}")
+	Architectures: $(join ', ' "${arches[@]}")
+EOE
+for arch in "${arches[@]}"; do
+	archVariant="${archLatest[$arch]}"
+	cat <<-EOA
+		${arch}-Directory: $archVariant
+	EOA
 done
