f2583bd2013d77140210bbde5942d6ddd3ec3b84
Tianon Gravi <admwiggin@gmail.com>
2018-01-22 10:05:37 -0800

    Use the builder image to detect whether we _can_ ping (QEMU user-mode networking, etc)

--- build.sh
+++ build.sh
@@ -18,10 +18,23 @@ for version in "${versions[@]}"; do
 		docker run --rm "$base$version-builder" tar cC rootfs . | xz -z9 > "$version/busybox.tar.xz"
 		docker build -t "$base$version-test" "$version"
 		docker run --rm "$base$version-test" sh -xec 'true'
-		if ! docker run --rm "$base$version-test" ping -c 1 google.com; then
-			sleep 1
-			docker run --rm "$base$version-test" ping -c 1 google.com
+
+		# detect whether the current host _can_ ping
+		# (QEMU user-mode networking does not route ping traffic)
+		shouldPing=
+		if docker run --rm "$base$version-builder" ping -c 1 google.com &> /dev/null; then
+			shouldPing=1
 		fi
+
+		if [ -n "$shouldPing" ]; then
+			if ! docker run --rm "$base$version-test" ping -c 1 google.com; then
+				sleep 1
+				docker run --rm "$base$version-test" ping -c 1 google.com
+			fi
+		else
+			docker run --rm "$base$version-test" nslookup google.com
+		fi
+
 		docker images "$base$version-test"
 	)
 done
