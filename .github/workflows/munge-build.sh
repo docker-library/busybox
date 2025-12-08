32603b7c30a9287e17d9d33da148dbc55e0ac27b
Tianon Gravi <admwiggin@gmail.com>
2025-12-08 10:23:49 -0800

    Add i386 builds to GHA (excluding uclibc)

--- .github/workflows/munge-build.sh
+++ .github/workflows/munge-build.sh
@@ -3,7 +3,10 @@ set -Eeuo pipefail
 
 jq '
 	.matrix.include |= map(
-		.runs.build = (
+		(.name | contains("i386")) as $i386
+		| if $i386 and (.name | contains("uclibc")) then empty else . end # uclibc builds are heavy; do not test i386/uclibc on GHA
+		| .BASHBREW_ARCH = (if $i386 then "i386" else "amd64" end)
+		| .runs.build = (
 			[
 				"dir=" + (.meta.entries[].directory | @sh),
 				"rm -rf \"$dir/$BASHBREW_ARCH\"", # make sure our OCI directory is clean so we can "git diff --exit-code" later
